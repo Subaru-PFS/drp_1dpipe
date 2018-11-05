6b50423b1b0d11b9fc3a468424ee4dc726b60cc6
Pierre-Yves Chabaud <pychabau@pcp-pchabaud.lam.fr>
2018-11-05 18:26:40 +0100

    New features: It's now possible to retrieve arg value from configuration file.
    See test_utils.py
    
    ---
    Add get_args_from_file function in drp_1dpipe.io.utils

--- drp_1dpipe/io/utils.py
+++ drp_1dpipe/io/utils.py
@@ -7,3 +7,18 @@ def get_auxiliary_path(dir):
 
 def get_conf_path(dir):
     return os.path.join(os.path.dirname(__file__), 'conf', dir)
+
+
+def get_args_from_file(file_name, args):
+    with open(file_name, 'r') as ff:
+        lines = ff.readlines()
+    for line in lines:
+        try:
+            key, value = line.replace('\n', '').split('#')[0].split("=")
+            try:
+                if getattr(args, key.strip()) is None:
+                    setattr(args, key.strip(), value.strip())
+            except AttributeError:
+                setattr(args, key.strip(), value.strip())
+        except ValueError:
+            pass
--- drp_1dpipe/tests/test_utils.py
+++ drp_1dpipe/tests/test_utils.py
@@ -14,7 +14,7 @@ def test_auxdir():
     """
     @brief The "test_auxdir" function.
     @details
-        This function test features concerning auxiliary directory.
+        This function tests features concerning auxiliary directory.
     """
     assert os.path.exists(get_auxiliary_path("."))
     assert not(os.path.exists(get_auxiliary_path("foo.txt")))
@@ -24,7 +24,45 @@ def test_confdir():
     """
     @brief The "test_confdir" function.
     @details
-        This function test features concerning configuration directory.
+        This function tests features concerning configuration directory.
     """
     assert os.path.exists(get_conf_path("."))
     assert not(os.path.exists(get_conf_path("foo.txt")))
+
+
+def test_args_from_file():
+    """
+    @brief The "test_args_from_file" function.
+    @details
+        This function tests feature of retrieving argument value from
+        configuration file
+    """
+    from drp_1dpipe.io.utils import get_args_from_file
+    import tempfile
+    fp1 = tempfile.NamedTemporaryFile()
+    conf_file = fp1.name
+    with open(conf_file, 'w') as cf:
+        cf.write('arg1 = 4\n')
+        cf.write('arg2 = foo2 foo2\n')
+        cf.write('arg3 = foo3 # test\n')
+        cf.write('arg4 = # foo4\n')
+        cf.write('arg5 # = foo5\n')
+        cf.write('#arg6 = foo6')
+        cf.write('arg7 arg7 = foo7\n')
+
+    class MyCls():
+        arg1 = "2"
+
+    args = MyCls()
+    get_args_from_file(conf_file, args)
+    assert args.arg1 == "2"
+    assert args.arg2 == "foo2 foo2"
+    assert args.arg3 == "foo3"
+    assert args.arg4 == ""
+    with pytest.raises(AttributeError):
+        getattr(args, "arg5")
+    with pytest.raises(AttributeError):
+        getattr(args, "arg6")
+    with pytest.raises(AttributeError):
+        getattr(args, "arg7")
+    fp1.close()
