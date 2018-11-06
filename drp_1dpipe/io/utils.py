492512dc0447004720573a2fd54dadaec2764643
Pierre-Yves Chabaud <pychabau@pcp-pchabaud.local>
2018-11-06 11:37:44 +0100

    Fix #5000 : * Unnest try block in io.utils.py : get_args_from_file function
    Fix partially #4999 :
    * Add docstring in io.utils

--- drp_1dpipe/io/utils.py
+++ drp_1dpipe/io/utils.py
@@ -3,12 +3,30 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def get_auxiliary_path(dir):
-    return os.path.join(os.path.dirname(__file__), 'auxdir', dir)
+def get_auxiliary_path(file_name):
+    """Return the full path of file in auxiliary directory
+
+    :param file_name: name of the file
+    :return: full path
+
+    :Example:
+
+    get_auxiliary_path("my_data.dat") # -> /python/package/path/my_data.dat
+    """
+    return os.path.join(os.path.dirname(__file__), 'auxdir', file_name)
 
 
-def get_conf_path(dir):
-    return os.path.join(os.path.dirname(__file__), 'conf', dir)
+def get_conf_path(file_name):
+    """Return the full path of file in configuration directory
+
+    :param file_name: name of the file
+    :return: full path
+
+    :Example:
+
+    get_conf_path("my_conf.conf") # -> /python/package/path/my_conf.conf
+    """
+    return os.path.join(os.path.dirname(__file__), 'conf', file_name)
 
 
 def init_logger(process_name):
@@ -29,15 +47,40 @@ def init_logger(process_name):
 
 
 def get_args_from_file(file_name, args):
+    """Get arguments value from configuration file
+
+    :param file_name: name of the configuration file
+    :param args: object (ex. retrieve from argparse module)
+
+    Get arguments value from configuration file. Value has to be formatted as
+    ``option = string``. To comment use ``#``.
+    The function modifies every ``None`` args attribue.
+    The function add new args attribute.
+
+    :Example:
+
+    In ``my_conf.conf`` file:
+    arg1 = 2
+    arg2 = foo
+
+    class MyCls():
+        arg1=1
+
+    args = MyCls()
+    get_args_from_file("my_conf.conf",args)
+
+    args.arg1 # -> 1
+    args.arg2 # -> foo
+    """
     with open(file_name, 'r') as ff:
         lines = ff.readlines()
     for line in lines:
         try:
             key, value = line.replace('\n', '').split('#')[0].split("=")
-            try:
-                if getattr(args, key.strip()) is None:
-                    setattr(args, key.strip(), value.strip())
-            except AttributeError:
-                setattr(args, key.strip(), value.strip())
         except ValueError:
-            pass
+            continue
+        try:
+            if getattr(args, key.strip()) is None:
+                setattr(args, key.strip(), value.strip())
+        except AttributeError:
+            setattr(args, key.strip(), value.strip())
