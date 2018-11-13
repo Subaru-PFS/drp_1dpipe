fb2953c8725512fab8a76cb86db0227d701644ce
Florian Fauchier <florian.fauchier@lam.fr>
2018-11-13 16:15:36 +0100

    up

--- drp_1dpipe/tests/test_pre_process.py
+++ drp_1dpipe/tests/test_pre_process.py
@@ -8,17 +8,41 @@ Author: PSF DRP1D developers
 import os
 import math
 import pytest
+import logging
 import tempfile
-from drp_1dpipe.pre_process.pre_process import main, bunch
+from drp_1dpipe.io.utils import init_logger
+from drp_1dpipe.pre_process.pre_process import run, bunch
+
+spectra_path = "/tmp/pfsssss/"
 
 
-def test_main():
+class FakeArgs(object):
+    def __init__(self):
+        """
+        The "test_main" function.
+
+        This function test the "bunch" function of "pre_process.py" module.
+        """
+        self.workdir = tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
+        self.logdir = tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
+        self.loglevel = 10
+        self.bunch_size = 4
+        self.spectra_path = tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
+        self.bunch_list = tempfile.NamedTemporaryFile(dir=spectra_path)
+
+
+def test_run():
     """
     The "test_main" function.
 
     This function test the "bunch" function of "pre_process.py" module.
     """
-    # TODO
+    args = FakeArgs()
+
+
+    result = run(args)
+    assert result is not None
+
 
 def test_bunch():
     """
