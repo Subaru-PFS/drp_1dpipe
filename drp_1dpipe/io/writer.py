14dcdd290def519310384bdebc68177447c734dd
Ali Allaoui <ali.allaoui@lam.fr>
2020-10-07 10:07:40 +0200

    Quality flag

--- drp_1dpipe/io/writer.py
+++ drp_1dpipe/io/writer.py
@@ -17,7 +17,8 @@ def write_candidates(output_dir,
               fits.Card('catId', catId, 'Source of the objId'),
               fits.Card('objId', objId, 'Unique ID for object'),
               fits.Card('nvisit', nVisit, 'Number of visit'),
-              fits.Card('vHash', pfsVisitHash, '63-bit SHA-1 list of visits')]
+              fits.Card('vHash', pfsVisitHash, '63-bit SHA-1 list of visits'),
+              fits.Card('ZWARNING',0,'Quality flag')]
 
     hdr = fits.Header(header)
     primary = fits.PrimaryHDU(header=hdr)
